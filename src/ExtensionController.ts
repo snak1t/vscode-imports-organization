4278ebebb20337b9f6b0face2e98bb3360ac5d78
Ruslan Abramov <r.o.abramov@gmail.com>
2020-07-03 23:30:08 +0300

    format inserted imports

--- src/ExtensionController.ts
+++ src/ExtensionController.ts
@@ -11,39 +11,57 @@ export class ExtensionController implements vscode.Disposable {
   disposables: vscode.Disposable[] = [];
 
   constructor(private readonly config: Config) {
-    let onDidSaveDisposable = vscode.workspace.onDidSaveTextDocument((textDocument: vscode.TextDocument) => {
-      if (this.isExtensionDisabledForDocument(textDocument)) {
+    const onDidSaveDisposable = vscode.workspace.onDidSaveTextDocument(async document => {
+      const edit = this.changeDocumentContent(document);
+      if (edit === null) {
         return;
       }
-      const textContent = textDocument.getText();
-      const file = toAst(textContent);
-      if (file === null) {
-        return;
-      }
-      const importNodes = getAllImportNodes(file);
-      const [shouldChangeContent, proccessedContent] = this.processImportNodes(file, importNodes);
-      if (!shouldChangeContent || !proccessedContent) {
-        return;
-      }
-      const wEdit = new vscode.WorkspaceEdit();
-      const lastImportsLineOfOriginalText = importNodes[importNodes.length - 1].getLinePositions()[1];
-      const range = new vscode.Range(
-        new vscode.Position(0, 0),
-        textDocument.lineAt(lastImportsLineOfOriginalText).range.end,
-      );
 
-      let eol = this.getCurrentEOL(textDocument);
-      if (textDocument.lineAt(lastImportsLineOfOriginalText + 1).isEmptyOrWhitespace) {
-        eol = "";
+      await this.applyEdit(document.uri, [edit]);
+      if (!this.config.isFormatOnSaveEnabled) {
+        const edits = (await vscode.commands.executeCommand(
+          "vscode.executeFormatRangeProvider",
+          document.uri,
+          edit.range,
+        )) as vscode.TextEdit[];
+        if (edits.length) {
+          await this.applyEdit(document.uri, edits);
+        }
       }
-      wEdit.replace(textDocument.uri, range, proccessedContent + eol);
-      vscode.workspace.applyEdit(wEdit).then(() => {
-        textDocument.save();
-      });
+      document.save();
     });
+
     this.registerCommands();
     this.disposables.push(onDidSaveDisposable);
   }
+
+  private changeDocumentContent = (textDocument: vscode.TextDocument): vscode.TextEdit | null => {
+    if (this.isExtensionDisabledForDocument(textDocument)) {
+      return null;
+    }
+    const textContent = textDocument.getText();
+    const file = toAst(textContent);
+    if (file === null) {
+      return null;
+    }
+    const importNodes = getAllImportNodes(file);
+    const [shouldChangeContent, proccessedContent] = this.processImportNodes(file, importNodes, textContent);
+    if (!shouldChangeContent || !proccessedContent) {
+      return null;
+    }
+    const lastImportsLineOfOriginalText = importNodes[importNodes.length - 1].getLinePositions()[1];
+    const range = new vscode.Range(
+      new vscode.Position(0, 0),
+      textDocument.lineAt(lastImportsLineOfOriginalText).range.end,
+    );
+
+    let eol = this.getCurrentEOL(textDocument);
+    if (textDocument.lineAt(lastImportsLineOfOriginalText + 1).isEmptyOrWhitespace) {
+      eol = "";
+    }
+
+    return vscode.TextEdit.replace(range, proccessedContent + eol);
+  };
 
   private getCurrentEOL(textDocument: vscode.TextDocument): string {
     return textDocument.eol === 1 ? "\n" : "\r\n";
@@ -91,11 +109,11 @@ export class ExtensionController implements vscode.Disposable {
     }
   };
 
-  private processImportNodes(file: File, importNodes: Node[]): [false] | [true, string] {
+  private processImportNodes(file: File, importNodes: Node[], initialCode: string): [false] | [true, string] {
     const nodes = sortImports(importNodes, this.config.getConfiguration(), this.config.getMixType());
     const hasChanged = hasImportsStructureChanged(importNodes, nodes);
     if (hasChanged) {
-      return [true, fromAst(replaceImportsWith(file, nodes))]; // ?
+      return [true, fromAst(replaceImportsWith(file, nodes ), initialCode)]; // ?
     }
     return [false];
   }
@@ -106,5 +124,11 @@ export class ExtensionController implements vscode.Disposable {
 
   dispose() {
     this.disposables.forEach(x => x.dispose());
+  }
+
+  private async applyEdit(uri: vscode.Uri, edits: vscode.TextEdit[]): Promise<boolean> {
+    const wEdit = new vscode.WorkspaceEdit();
+    wEdit.set(uri, edits);
+    return await vscode.workspace.applyEdit(wEdit);
   }
 }
